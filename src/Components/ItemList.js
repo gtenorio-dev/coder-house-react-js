6495a1ac87dd3a50dbef59bbc7de8130ef15b330
gtenorio-dev <gtenorio.dev@gmail.com>
2022-03-23 04:38:47 -0300

    Fix bug using Hooks

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,64 +1,12 @@
 import React from "react";
 import Item from "./Item";
 
-const products = [
-  {
-    id: 1,
-    name: "Copper Dispenser Laminado",
-    description: "Descripcion 1",
-    price: 1960,
-    stock: 6,
-  },
-  {
-    id: 2,
-    name: "Copper Jabonera Laminado",
-    description: "Descripcion 2",
-    price: 1070,
-    stock: 8,
-  },
-  {
-    id: 3,
-    name: "Bandeja Eucalipto Natural",
-    description: "Descripcion 3",
-    price: 2299.99,
-    stock: 16,
-  },
-];
-
-const productsService = new Promise((res, err) => {
-  setTimeout(function () {
-    console.log("Request to API (2 seg)");
-    res(products);
-  }, 2000);
-});
-
-const getProducts = () => {
-  productsService.then(
-    (res) => {
-      console.log(res);
-      return res;
-    },
-    (err) => {
-      console.log("Error!");
-    }
-  );
-};
-
-export const ItemList = () => {
+export const ItemList = ({products}) => {
   return (
     <div>
       {products.map((p) => (
         <Item product={p} />
       ))}
     </div>
-
-    // **** COMO HAGO PARA QUE MUESTRE LA LISTA DESPUES DE LOS 2 SEGUNDOS? **** 
-    // <div>
-    //   {productsService.then((res) => {
-    //     res.map((p) => {
-    //       <Item product={p} />;
-    //     });
-    //   })}
-    // </div>
   );
 };
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
-import {ItemList} from "./ItemList";
+import { ItemList } from "./ItemList";
+
+const productList = [
+  {
+    id: 1,
+    name: "Copper Dispenser Laminado",
+    description: "Descripcion 1",
+    price: 1960,
+    stock: 6,
+  },
+  {
+    id: 2,
+    name: "Copper Jabonera Laminado",
+    description: "Descripcion 2",
+    price: 1070,
+    stock: 8,
+  },
+  {
+    id: 3,
+    name: "Bandeja Eucalipto Natural",
+    description: "Descripcion 3",
+    price: 2299.99,
+    stock: 16,
+  },
+];
+
+const getProducts = new Promise((res, err) => {
+  setTimeout(() => {
+    console.log("Request to API (2 seg)");
+    res(productList);
+  }, 2000);
+});
 
 const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts
+      .then((res) => setProducts(res))
+      .catch((err) => console.log("Error"));
+  }, []);
+
   return (
     <div>
       {/* <ProductItem /> */}
-      <ItemList />
+      <ItemList products={products} />
     </div>
   );
 };
